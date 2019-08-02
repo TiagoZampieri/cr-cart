09c5c5603b6f999b915e24f3fd373ebd269c64b3
zampieri <tzampieri7@gmail.com>
2019-08-02 13:18:57 -0300

    [CARINHO] - Estrutura html do carrinho.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { CartListComponent } from './cart-list/cart-list.component';
 
 @NgModule({
-  declarations: [ProductCardComponent],
+  declarations: [
+    ProductCardComponent,
+    CartListComponent
+  ],
   imports: [
     CommonModule
   ],
-  exports: [ProductCardComponent]
+  exports: [
+    ProductCardComponent,
+    CartListComponent
+  ]
 })
 export class ComponentsModule { }
